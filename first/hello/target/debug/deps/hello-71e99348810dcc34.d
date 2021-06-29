36a0d727d177bba32aa22ed64aff385cd7f0d4cf
tur461 <tur461.er@gmail.com>
2021-06-29 22:00:07 +0530

    added...

---
+++ first/hello/target/debug/deps/hello-71e99348810dcc34.d
@@ -0,0 +1,5 @@
+/home/lenovo/projects/per/rust/first/hello/target/debug/deps/hello-71e99348810dcc34: src/main.rs
+
+/home/lenovo/projects/per/rust/first/hello/target/debug/deps/hello-71e99348810dcc34.d: src/main.rs
+
+src/main.rs:
